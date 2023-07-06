4af2fdb3d33dc9a7547bbe116f71f355259f6d2e
Markloev <markloev@gmail.com>
2023-07-06 11:49:09 -0500

    Update index.ts (#11)

--- cloud_clipboard/src/index.ts
+++ cloud_clipboard/src/index.ts
@@ -18,7 +18,7 @@ interface Config {
 }
 
 const CORS_HEADERS = {
-  "Access-Control-Allow-Origin": "https://remix.app",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET,PUT,HEAD,POST,OPTIONS",
   "Access-Control-Max-Age": "86400"
 };
